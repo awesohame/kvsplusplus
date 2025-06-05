c3967e31686f59c315d41fee49bc6162403b88f6
Awesohame <82087247+awesohame@users.noreply.github.com>
2025-06-05 14:18:11 +0530

    improve helper formatting

--- src/utils/Helpers.cpp
+++ src/utils/Helpers.cpp
@@ -1,20 +1,20 @@
 #include "kvstore/utils/Helpers.hpp"
 
 namespace kvspp {
-namespace utils {
+    namespace utils {
 
-// ANSI color code definitions
-const char* ColorOutput::COLOR_GREEN = "\033[32m";
-const char* ColorOutput::COLOR_RED = "\033[31m";
-const char* ColorOutput::COLOR_RESET = "\033[0m";
+        // ANSI color code definitions
+        const char* ColorOutput::COLOR_GREEN = "\033[32m";
+        const char* ColorOutput::COLOR_RED = "\033[31m";
+        const char* ColorOutput::COLOR_RESET = "\033[0m";
 
-std::string ColorOutput::passMsg(const std::string& message) {
-    return std::string(COLOR_GREEN) + "[PASS]" + COLOR_RESET + " " + message;
+        std::string ColorOutput::passMsg(const std::string& message) {
+            return std::string(COLOR_GREEN) + "[PASS]" + COLOR_RESET + " " + message;
+        }
+
+        std::string ColorOutput::failMsg(const std::string& message) {
+            return std::string(COLOR_RED) + "[FAIL]" + COLOR_RESET + " " + message;
+        }
+
+    }
 }
-
-std::string ColorOutput::failMsg(const std::string& message) {
-    return std::string(COLOR_RED) + "[FAIL]" + COLOR_RESET + " " + message;
-}
-
-} // namespace utils
-} // namespace kvspp
